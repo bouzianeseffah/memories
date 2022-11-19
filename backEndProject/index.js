0da1063d5404f6b1b32bbde06057350475709190
bouzianes <fcbdreams1991@gmail.com>
2022-11-19 12:02:41 -0500

    connecting to mangoo

--- backEndProject/index.js
+++ backEndProject/index.js
@@ -4,12 +4,22 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 //initialize the app 
 const app = express();
+const PORT = process.env.PORT || 5000
+const CONNECTION_URL = process.env.CONNECTION_URL
+
 
 //limit:30mp in case of image large size
 app.use(bodyParser.json({limit: '30mp', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mp', extended: true}));
 app.use(cors());
-//cloud atlas version of mangoodb 
+
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, ()=> console.log(`successful connected on${PORT}`)))
+.catch((err) => console.log(err))
+
