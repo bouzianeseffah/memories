838dcb11d0dd70320fd1d62459a62f0191290d02
bouzianes <fcbdreams1991@gmail.com>
2022-11-23 19:58:36 -0500

    adding styles

--- frontendproject/src/components/Posts/Posts.js
+++ frontendproject/src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Post from "./Post/post";
+import useStyles from './styles'
 
 const Posts = () => {
+    const classes = useStyles()
     return(
         <>
            <h1>Posts</h1>
